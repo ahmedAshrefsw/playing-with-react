bd4636f23d7cb23dd7278c8e711a381f1f448bb1
ahmedAshref <58373471+ahmedAshrefsw@users.noreply.github.com>
2021-03-30 06:37:44 +0200

    building todo, wallpaper app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './NavBar';
-import Home from './Home'
-// app is the root component ! in react app 
-// component tree starts with app component 
-// every other component should be nested in the app component and so on 
-// component is just a fun that return a jsx code and export it at the end of the file 
- 
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./NavBar";
+import Home from "./Home";
+import ToDo from "./ToDo";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// app is the root component ! in react app
+// component tree starts with app component
+// every other component should be nested in the app component and so on
+// component is just a fun that return a jsx code and export it at the end of the file
+
 function App() {
-  const name = "Ahmed";
-
-
-
   return (
     // className is alternative to html calss att in jsx
-    <div className="App">
-      {/*  // here is nav bar react js component nested in the main component */}
-      <Navbar /> 
-      <Home />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/todo">
+              <ToDo />
+            </Route>
+            <Route exact path="/about">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
